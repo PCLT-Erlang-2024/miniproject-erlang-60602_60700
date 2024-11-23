8fdfc3b00bbf561e72d6240a8da91d877ddfa2d1
RafaelMartins4 <rlo.martins@campus.fct.unl.pt>
2024-11-23 12:48:08 +0000

    Small changes to task 1; Added task 1 report

--- task1/task1.erl
+++ task1/task1.erl
@@ -9,16 +9,16 @@ load_truck(MAX_CAPACITY, CAPACITY, TRUCK_ID, N_TRUCKS) ->
             if
                 NEW_CAPACITY < 0 ->
                     io:format("Truck ~p is full and was dispatched!~n",[TRUCK_ID]), 
-                    io:format("Package with size ~p was loaded onto Truck ~p!~n",[SIZE, TRUCK_ID + N_TRUCKS]),
+                    io:format("Package with size ~p was loaded onto Truck ~p which now has ~p leftover capacity!~n",[SIZE, TRUCK_ID + N_TRUCKS, MAX_CAPACITY - SIZE]),
                     load_truck(MAX_CAPACITY,MAX_CAPACITY-SIZE,TRUCK_ID + N_TRUCKS,N_TRUCKS);
 
                 NEW_CAPACITY == 0 -> 
-                    io:format("Package with size ~p was loaded onto Truck ~p!~n",[SIZE, TRUCK_ID]),
+                    io:format("Package with size ~p was loaded onto Truck ~p which now has ~p leftover capacity!~n",[SIZE, TRUCK_ID, NEW_CAPACITY]),
                     io:format("Truck ~p is full and was dispatched!~n",[TRUCK_ID]),
                     load_truck(MAX_CAPACITY,MAX_CAPACITY,TRUCK_ID + N_TRUCKS,N_TRUCKS);
 
                 NEW_CAPACITY > 0 -> 
-                    io:format("Package with size ~p was loaded onto Truck ~p!~n",[SIZE, TRUCK_ID]),
+                    io:format("Package with size ~p was loaded onto Truck ~p which now has ~p leftover capacity!~n",[SIZE, TRUCK_ID, NEW_CAPACITY]),
                     load_truck(MAX_CAPACITY,NEW_CAPACITY,TRUCK_ID,N_TRUCKS)
             end;
         stop -> io:format("Loading Truck ~p stopped!",[TRUCK_ID])
@@ -32,12 +32,12 @@ producer(CONVEYOR,PCK_SIZE) ->
 .
     
 
-spawner(N_CONVEYORS,N_CONVEYORS,MAX_CAPACITY,PCK_SIZE) -> 0;
-spawner(CONVEYOR_ID,N_CONVEYORS,MAX_CAPACITY,PCK_SIZE) ->
-    C = spawn(?MODULE,load_truck,[MAX_CAPACITY,MAX_CAPACITY,CONVEYOR_ID,N_CONVEYORS]),
+spawner(N_TRUCKS,N_TRUCKS,_,_) -> io:format("All threads have been created!~n");
+spawner(TRUCK_ID,N_TRUCKS,MAX_CAPACITY,PCK_SIZE) ->
+    C = spawn(?MODULE,load_truck,[MAX_CAPACITY,MAX_CAPACITY,TRUCK_ID,N_TRUCKS]),
     spawn(?MODULE,producer,[C,PCK_SIZE]),
-    spawner(CONVEYOR_ID + 1,N_CONVEYORS, MAX_CAPACITY, PCK_SIZE)
+    spawner(TRUCK_ID + 1,N_TRUCKS, MAX_CAPACITY, PCK_SIZE)
 .
 
-start(N_CONVEYORS,MAX_CAPACITY,PCK_SIZE) -> spawner(0, N_CONVEYORS, MAX_CAPACITY, PCK_SIZE).
+start(N_TRUCKS,MAX_CAPACITY,PCK_SIZE) -> spawner(0, N_TRUCKS, MAX_CAPACITY, PCK_SIZE).
 
